f667d541043663e2d31d11235bdd398ce959dd19
th3pc7 <zPadidaSz@gmail.com>
2016-10-07 15:54:19 +0700

    UPDATE table user - add feild percent

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
+            $table->decimal('percent', 9, 6); // เครดิตของ account
             $table->decimal('credit', 16, 6); // เครดิตของ account
             $table->decimal('balance', 16, 6); // ยอดได้เสียของ account
             $table->integer('agent_id')->nullable(); // id ของเอเย่น account
@@ -47,6 +48,7 @@ class CreateUsersTable extends Migration
                 'name' => 'Admin_lotto',
                 'email' => 'admin',
                 'password' => md5('Password'),
+                'percent' => '100',
                 'credit' => '1000000000',
                 'balance' => '0',
                 'user_map' => '1'
